98e6179311aa98d2362168cf2173ddf8c023facd
karwanm <karwan.ismo@gmail.com>
2022-06-28 21:57:06 +0100

    add register component and header's tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-function App() {
+import Header from "./components/Header";
+import DailyLog from "./components/DailyLog";
+import History from "./components/History";
+import Register from "./components/Register";
+
+const App = () => {
   return (
     <div className="App">
-      Tiney
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<DailyLog />} />
+          <Route path="/" element={<History />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
  const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <Link to="/dailylog">DailyLog</Link>
+      <Link to="/dailylog">History</Link>
+    </div>
   )
 }
 
