4c81c1b0ffd7c833bd0cb8c236c42bb251cf7323
karwanm <karwan.ismo@gmail.com>
2022-06-30 15:21:40 +0100

    access Data from json file

--- src/components/DailyLog.js
+++ src/components/DailyLog.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Data from "../data.json";
 
 const DailyLog = () => {
+  const [children, setChildren] = useState([]);
+
+  useEffect(() => {
+    setChildren(Data.children);
+  }, []);
 
   return (
     <div className="dayli-log">
+      <ul>
+        {children ? <p>{children.map((child) => {
+          return <li key={child.id}>{child.firstName}</li>
+        })}</p> : <p>Loadding...</p>}
+      </ul>
     </div>
   );
 };
