cc0e98197779339a9521d91c082cb82b510868e9
karwanm <karwan.ismo@gmail.com>
2022-06-30 21:27:44 +0100

    add signin and out function and fetch data from json file

--- src/components/DailyLog.js
+++ src/components/DailyLog.js
@@ -1,12 +1,53 @@
 import React, { useState, useEffect } from "react";
-import Data from "../data.json";
+import Data from "../db.json";
 
 const DailyLog = () => {
   const [children, setChildren] = useState([]);
 
+  const newDate = new Date();
+
   useEffect(() => {
     setChildren(Data.children);
+
+    try {
+      fetch("http://localhost:4000/children")
+        .then((res) => res.json())
+        .then((data) => {
+          setChildren(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:4000/children/" + e.target.value, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: e.target.value,
+        signedIn: true,
+        hour: newDate.getHours(),
+        minute: newDate.getMinutes(),
+      }),
+    });
+  };
+
+  const handleSignOut = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:4000/children/" + e.target.value, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: e.target.value,
+        signedIn: false,
+      }),
+    });
+  };
 
   return (
     <div className="daily-log">
@@ -20,10 +61,25 @@ const DailyLog = () => {
                   <img src={child.src} />
                   <div className="name">
                     <p>{child.firstName}</p>
-                    <span>Signed out</span>
+                    {child.signedIn ? (
+                      child.hour.length > 2 ? (
+                        <span>Signed in at 0{child.hour}:{child.minute}</span>
+                      ) : (
+                        <span>Signed in at {child.hour}:{child.minute}</span>
+                      )
+                    ) : (
+                      <span>Signed out</span>
+                    )}
                   </div>
-
-                  <button>Sign in</button>
+                  {child.signedIn ? (
+                    <button value={child.id} className="signed-in" onClick={handleSignOut}>
+                      Sign out
+                    </button>
+                  ) : (
+                    <button value={child.id} className="signed-out" onClick={handleSignIn}>
+                      Sign in
+                    </button>
+                  )}
                 </li>
               );
             })}
